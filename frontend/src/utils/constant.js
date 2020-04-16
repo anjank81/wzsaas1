ad51dd387413b78a5b878f3b19c9f7b7b5854689
Anjan J Kumar <anjank81@gmail.com>
2020-04-16 21:01:34 +0530

    Link change

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
 import * as Cookie from "./Cookie";
-export const URL = "http://localhost"; //http://ec2-54-179-146-232.ap-southeast-1.compute.amazonaws.com http://localhost
-export const API_URL_ROOT = `${URL}:4000`;
+export const URL = "http://ec2-54-179-146-232.ap-southeast-1.compute.amazonaws.com"; //http://ec2-54-179-146-232.ap-southeast-1.compute.amazonaws.com http://localhost
+export const API_URL_ROOT = `${URL}:3000`;
 export const ACCESS_TOKEN = "accesToken";
 export const USER_DETAILS = "userDetails";
 export const SUCCESS = "SUCCESS";
